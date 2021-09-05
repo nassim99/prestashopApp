aa60fc1217e30fc479f5b432e007d93f6b64a436
Redwan Nassim <redoin10@gmail.com>
2021-09-05 22:44:15 +0100

    add absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,38 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  if (process.env['ENV'] === 'prod') {
+    console.log(process.env['ENV']);
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      //['transform-remove-console'],
+      [
+        'inline-dotenv',
+        {
+          path: './.env', // See motdotla/dotenv for more options
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json', '.svg', '.png', '.jpg'],
+          alias: {
+            assets: './src/assets',
+            components: './src/components',
+            atoms: './src/components/atoms',
+            molecules: './src/components/molecules',
+            organisms: './src/components/organisms',
+            navigations: './src/navigations',
+            screens: './src/screens',
+            services: './src/services',
+            styles: './src/styles',
+            hooks: './src/hooks',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
+  };
 };
